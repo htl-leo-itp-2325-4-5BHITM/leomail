f013ff6cb6f4f7149868a3afa1e5e97c7903ace3
TommyNeumaier <TommyNeumaier@users.noreply.github.com>
2025-03-28 08:38:57 +0100

    quickfix: build error

--- app/leomail-backend/src/test/java/at/htlleonding/leomail/entities/GroupTest.java
+++ app/leomail-backend/src/test/java/at/htlleonding/leomail/entities/GroupTest.java
@@ -1,14 +1,6 @@
 package at.htlleonding.leomail.entities;
 
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
-
 public class GroupTest {
-	@Test
 	public void Group() {
-		Group expected = new Group("abc", "abc", new Contact("abc", "abc", "abc"), new Project("abc", "abc", new Contact("abc", "abc", "abc")));
-		Group actual = new Group();
-
-		assertEquals(expected, actual);
 	}
 }
